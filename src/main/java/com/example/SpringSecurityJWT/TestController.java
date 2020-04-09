a2b63904e9465e602edfadc765ebf0e3c298047c
hitoamitkumar <hitoamitkumar@gmail.com>
2020-04-09 22:32:29 +0530

    initial commit

---
+++ src/main/java/com/example/SpringSecurityJWT/TestController.java
@@ -0,0 +1,15 @@
+package com.example.SpringSecurityJWT;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@RequestMapping(value="/")
+	public String add()
+	{
+		return "Done";
+	}
+
+}
